4eca9504167df1cd38c8505903e1b7c1b8160c3c
yosse95ai <karateyosi@gmail.com>
2021-01-22 22:47:41 +0900

    Ad: OpenCLPrac7_6_1

---
+++ code/OpenCLPrac7_6_1.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Drawing;
+#if true
+partial class Program
+{
+    private const string clProc = "clCode";
+
+    //----------------------------------------------------------------------
+    // createOutBytes                  /*Size of ImageObject after change.*/
+    static byte[] createOutBytes(int width, int height,
+        out int outWidth, out int outHeight, float scale)
+    {
+        outWidth = (int)Math.Floor((double)width * scale);
+        outHeight = (int)Math.Floor((double)height * scale);
+
+        byte[] outBytes = new byte[outWidth * 4 * outHeight];
+
+        return outBytes;
+    }
+    //----------------------------------------------------------------------
+    // effct
+    static Bitmap effect(string clName, byte[] rgbMat,
+        int width, int height, float param)
+    {
+        string[] src = { System.IO.File.ReadAllText(clName) };
+
+        int outWidth, outHeight;
+        byte[] outRgb = createOutBytes(width, height,
+            out outWidth, out outHeight, param);
+
+
+        IntPtr[] platformId = GetPlatformIDs();                 // get platform id
+        IntPtr[] deviceID = GetDeviceIDs(platformId);           // get device id
+        IntPtr context = CreateContext(platformId, deviceID);   // create Context
+        IntPtr queue = CreateCommandQueue(context, deviceID[0]);// create CommandQueue
+        IntPtr prog = CreatePragramWithSource(context, src);    // create program object
+        BuildPragram(prog, deviceID);                           // build program
+        IntPtr kernel = CreateKernel(prog, clProc);             // create kernel
+
+        // set image format
+        cl_image_format format = new cl_image_format(CL_BGRA, CL_UNORM_INT8);
+        cl_image_desc idesc = new cl_image_desc(CL_MEM_OBJECT_IMAGE2D,
+            (IntPtr)width, (IntPtr)height);
+
+        IntPtr memIn = CreateImage(context, rgbMat,
+            CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, ref format, ref idesc);
+
+        cl_image_desc odesc = new cl_image_desc(CL_MEM_OBJECT_IMAGE2D,
+            (IntPtr)outWidth, (IntPtr)outHeight);
+
+        IntPtr memOut = CreateImage(context,
+            CL_MEM_WRITE_ONLY, ref format, ref odesc);
+
+        IntPtr[] origin = { (IntPtr)0, (IntPtr)0, (IntPtr)0 };  // obtain results
+        IntPtr[] region = { (IntPtr)outWidth, (IntPtr)outHeight, (IntPtr)1 };
+
+        SetKernelAeg(kernel, 0, memIn);                      // set kerbel parameters
+        SetKernelAeg(kernel, 1, memOut);
+        SetKernelAeg(kernel, 2, param);
+
+        IntPtr[] globalSize = { (IntPtr)outWidth, (IntPtr)outHeight };
+        EnqueueNDRangeKernel(queue, kernel, 2, globalSize);   // request execute kernel
+
+        EnqueueReadImage(queue, memOut, outRgb, origin, region);
+
+        clReleaseMemObject(memIn);                              // release resources
+        clReleaseMemObject(memOut);
+        clReleaseKernel(kernel);
+        clReleaseProgram(prog);
+        clReleaseCommandQueue(queue);
+        clReleaseContext(context);
+
+        return byteArray2bmp(outRgb, (int)outWidth, (int)outHeight);
+    }
+
+
+
+    //----------------------------------------------------------------------
+    // Main
+    static void Main(string[] args)
+    {
+        try
+        {
+            if (args.Length != 4)                                   // check arguments
+                throw new Exception("引数に以下の値を指定してください．" +
+                    " <入力ファイル> <出力ファイル> <*.cl> <倍率>");
+            Bitmap inBmp = readBmp(args[0]);
+
+            Byte[] rgbMat = bmp2byteArray(inBmp);
+
+            float param = Convert.ToSingle(args[3]);
+
+            using (Bitmap bmp = effect(args[2], rgbMat,
+                inBmp.Width, inBmp.Height, param))
+            {
+                rgbMat = null;
+                inBmp.Dispose();
+
+                writeBmp(bmp, args[1]);
+            };
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("{0}", ex.ToString());
+        }
+    }
+}
+
+#endif
